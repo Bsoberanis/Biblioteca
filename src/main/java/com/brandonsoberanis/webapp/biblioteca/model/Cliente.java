245c8055a8ad4f99a0345d1f5d5fc620f9a6a296
Bsoberanis <Bsoberanis-2023433@kinal.edu.gt>
2024-08-07 15:03:50 -0600

    carga autorizada

--- src/main/java/com/brandonsoberanis/webapp/biblioteca/model/Cliente.java
+++
@@ -1,5 +0,0 @@
-package com.brandonsoberanis.webapp.biblioteca.model;
-
-public class Cliente {
-
-}
--- src/main/java/com/brandonsoberanis/webapp/biblioteca/repository/LibroRepocitory.java
+++
@@ -1,8 +0,0 @@
-package com.brandonsoberanis.webapp.biblioteca.repository;
-
-import org.springframework.data.jpa.respository.JpaRepository;
-import org.brandonsoberanis.webapp.biblioteca.model.Libro;
-
-public interface LibroRepocitory extends JpaRepository<Libro, long>{
-
-}
